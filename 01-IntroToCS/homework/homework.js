e86c4e1322a718772aa7f032eb3825f7c72ba9e5
auccapuclla <auccapuclla2013@gmail.com>
2022-05-24 03:25:38 +0800

    .gitignore and 01-IntroToCS hw solution added

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,27 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  const len = num.length;
+  let numDec = 0;
+  [...num].forEach(
+    (elem, index) => (numDec += parseInt(elem) * Math.pow(2, len - 1 - index))
+  );
+  return numDec;
 }
-
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var num = parseInt(num);
+  let numBin = [];
+  while (Math.floor(num / 2) !== 0) {
+    numBin.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  numBin.unshift(num);
+  return numBin.join("");
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
